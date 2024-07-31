de95c87385a2b4ab8e739411bc1799da1961c30f
Mac21 <russ.tolle@gmail.com>
2024-07-31 02:13:21 -0600

    Add extra debugging for when a hand is submitted

--- index_handlers.go
+++ index_handlers.go
@@ -34,8 +34,14 @@ func runningGameHandler(c *gin.Context) {
 	pickedScore, _ := c.GetPostForm("chosenscore")
 	hand1score, _ := c.GetPostForm("hand1score")
 	hand2score, _ := c.GetPostForm("hand2score")
+	boardstr, _ := c.GetPostForm("board")
+	hand1str, _ := c.GetPostForm("hand1")
+	hand2str, _ := c.GetPostForm("hand2")
 
-	fmt.Println(pickedScore, hand1score, hand2score, overallScore)
+    fmt.Printf("Board: %s\n", boardstr)
+    fmt.Printf("\tHand1: %s\n", hand1str)
+    fmt.Printf("\tHand2: %s\n", hand2str)
+    fmt.Printf("\tpicked score: %s, hand1score: %s, hand2score: %s, overallscore: %s\n", pickedScore, hand1score, hand2score, overallScore)
 
 	switch pickedScore {
 	case "hand1":
